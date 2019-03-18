c61ca8b8990f445d8b5c66f291e1a4b4a73d0cb5
David Charles <ratlover03@gmail.com>
2019-03-18 01:16:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "select authors.name, subgenres.name from authors
-   join series on series.author_id = authors.id "
+   join series on series.author_id = authors.id join subgenres
+   on series.subgenre-id = subgenre.id;"
 end
 
 def select_series_title_with_most_human_characters

106b354bfc1ec9515b365507a0d3f45ff42ae703
David Charles <ratlover03@gmail.com>
2019-03-18 00:52:05 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -27,4 +27,21 @@ create table authors (
 create table books (
   id integer primary key,
   title text,
-)
+  year integer,
+  series_id integer
+);
+
+create table characters (
+  id integer primary key,
+  name text,
+  species text,
+  motto text,
+  series_id integer,
+  author_id integer
+);
+
+create table character_books (
+  id integer primary key,
+  book_id integer,
+  character_id integer
+);

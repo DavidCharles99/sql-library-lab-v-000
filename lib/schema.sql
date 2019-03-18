524d921caf7c1d04564ebce33c62a5960349f3f2
David Charles <ratlover03@gmail.com>
2019-03-18 01:19:48 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,5 @@
 create table characters (
-  id integer primary key,
+  id INTEGER PRIMARY KEY,
   name text,
   motto text,
   species text,
@@ -8,31 +8,31 @@ create table characters (
 );
 
 create table series (
-  id integer primary key,
+  id INTEGER PRIMARY KEY,
   title text,
   author_id integer,
   subgenre_id integer
 );
 
 create table subgenres (
-  id integer primary key,
+  id INTEGER PRIMARY KEY,
   name text
 );
 
 create table authors (
-  id integer primary key,
+  id INTEGER PRIMARY KEY,
   name text
 );
 
 create table books (
-  id integer primary key,
+  id INTEGER PRIMARY KEY,
   title text,
   year integer,
   series_id integer
 );
 
 create table character_books (
-  id integer primary key,
+  id INTEGER PRIMARY KEY,
   book_id integer,
   character_id integer
 );

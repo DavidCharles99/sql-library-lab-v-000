5aba6188128e5778807c386a6a528fc39d1b73ed
David Charles <ratlover03@gmail.com>
2019-03-18 00:56:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select species, count (*) from characters group by species
+  order by count (species) desc limit 1"
 end
 
 def select_name_and_series_subgenres_of_authors
